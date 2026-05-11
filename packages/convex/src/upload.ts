e617afdc80eca125fb070d959dcbb06ecc2d9a7c
nucleus48 <nucleuserumagborie@gmail.com>
2026-05-11 16:59:28 +0100

    feat(ltx-2.3-pod): install necessary tools in pod container

--- packages/convex/src/upload.ts
+++ packages/convex/src/upload.ts
@@ -1,7 +1,12 @@
 /* eslint-disable no-restricted-imports */
-import { mutation } from "./_generated/server";
+import { httpAction, mutation } from "./_generated/server";
 
 export const generateUrl = mutation({
   args: {},
   handler: (ctx) => ctx.storage.generateUploadUrl(),
+});
+
+export const generateUploadUrl = httpAction(async (ctx) => {
+  const url = await ctx.storage.generateUploadUrl();
+  return Response.json({ url });
 });
